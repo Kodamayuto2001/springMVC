67a258b27c499630675aa7e3499c77be6e975757
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2022-02-12 17:30:14 +0900

    #1 modified creating ContactDAO

--- src/com/yk/contact/dao/ContactDAO.java
+++ src/com/yk/contact/dao/ContactDAO.java
@@ -1,5 +1,17 @@
 package com.yk.contact.dao;
 
-public interface ContactDAO {
+import java.util.List;
 
+import com.yk.contact.model.Contact;
+
+public interface ContactDAO {
+	public int save(Contact contact);
+
+	public int update(Contact contact);
+
+	public Contact get(Integer id);
+
+	public int delete(Integer id);
+
+	public List<Contact> list();
 }
