b723610b47970e54971e8f168539bf4de4c76331
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2022-02-12 16:20:05 +0900

    fix Value Object

--- src/com/yk/contact/model/Contact.java
+++ src/com/yk/contact/model/Contact.java
@@ -1,22 +1,80 @@
 package com.yk.contact.model;
 
-import lombok.Setter;
-import lombok.Getter;
+//import lombok.Setter;
+//import lombok.Getter;
+
+//public class Contact {
+//	@Getter,@Setter
+//	private Interger id;
+//
+//	@Getter,@Setter
+//	private String name;
+//
+//	@Getter,@Setter
+//	private String email;
+//
+//	@Getter,@Setter
+//	private String address;
+//
+//	@Getter,@Setter
+//	private String phone;
+//}
+
 
 public class Contact {
-	@Getter,@Setter
-	private Interger id;
-
-	@Getter,@Setter
+	private Integer id;
 	private String name;
-
-	@Getter,@Setter
 	private String email;
-
-	@Getter,@Setter
 	private String address;
-
-	@Getter,@Setter
 	private String phone;
-}
 
+	protected Contact(Integer id, String name, String email, String address, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.phone = phone;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
